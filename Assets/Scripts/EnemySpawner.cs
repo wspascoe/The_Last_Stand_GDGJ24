568a004a15a6a2d99bbbb53bd56a3e1c3e1accd3
wspascoe <wspascoe@gmail.com>
2024-06-01 15:42:10 -0400

    combat Setup

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping;
     WaveConfigSO currentWave;
 
 
@@ -21,19 +22,24 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i),
-                            currentWave.GetStartingWaypoint().position,
-                            Quaternion.identity,
-                            transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                                currentWave.GetStartingWaypoint().position,
+                                Quaternion.identity,
+                                transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
+
             }
             yield return new WaitForSeconds(timeBetweenWaves);
-
         }
+        while (isLooping);
     }
 }
