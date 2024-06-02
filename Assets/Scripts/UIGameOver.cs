605c17b63b73deb2451bb13e02829eba9d8bf5b4
wspascoe <wspascoe@gmail.com>
2024-06-02 10:33:05 -0400

    Finished Project

---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    void Start()
+    {
+        scoreText.text = scoreKeeper.GetScore().ToString();
+    }
+}
